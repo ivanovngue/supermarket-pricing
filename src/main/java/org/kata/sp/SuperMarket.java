95e4b14b916cf1b32e7124bb434db458fe0a5d44
ivanovngue <53440724+ivanovngue@users.noreply.github.com>
2021-09-22 18:34:05 +0400

    First commit to initialize project

---
+++ src/main/java/org/kata/sp/SuperMarket.java
@@ -0,0 +1,12 @@
+package org.kata.sp;
+
+/**
+ * This is the main class
+ *
+ * @author Ivan
+ */
+public class SuperMarket {
+    public static void main(String[] args){
+        System.out.println("Supermarket Pricing");
+    }
+}
